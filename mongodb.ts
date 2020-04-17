c2c0b146d9555bfc19cb156d0cb937d5fea81bf1
Aaron Quang <48296934+Internlol@users.noreply.github.com>
2020-04-17 19:23:04 -0400

    Update mongodb.ts

--- mongodb.ts
+++ mongodb.ts
@@ -1,14 +1,12 @@
 export class Database {
 
     private MongoClient = require('mongodb').MongoClient;
-    private uri = "mongodb+srv://guest:guest@cluster0-y0tyl.mongodb.net/test?retryWrites=true&w=majority";
     private client;
     private collectionName : string;
     private dbName : string = "test";
 
     constructor(collectionName) {
 		this.collectionName = collectionName;
-		this.client = new this.MongoClient(this.uri, { useNewUrlParser: true });
 		// Open up a connection to the client.
 		// The connection is asynchronous, but we can't call await directly
 		// in the constructor, which cannot be async. So, we use "IIFE". Explanation below.
