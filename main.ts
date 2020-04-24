901e2969e53b237ad46f81e24036d716a5a65627
arnold-j <arnoldjoseph@gmail.com>
2020-04-24 16:38:42 -0400

    tsconfig

--- main.ts
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-import { Database } from './mongodb';
-import { MyServer } from './server-routing';
-
-const theDatabase = new Database('testDB');
-const theServer = new MyServer(theDatabase);
-
-theServer.listen(8080);
