082a1d4124d52347b675af2a35e871717dc1f372
Aaron Quang <48296934+Internlol@users.noreply.github.com>
2020-04-24 17:34:18 -0400

    port

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ const mongodb_1 = require("./mongodb");
 const server_routing_1 = require("./server-routing");
 const theDatabase = new mongodb_1.Database('test'); // CHANGE THIS
 const theServer = new server_routing_1.MyServer(theDatabase);
-theServer.listen(8080);
+theServer.listen(process.env.PORT);
--- server.ts
+++ server.ts
@@ -6,4 +6,4 @@ import { MyServer } from './server-routing';
 const theDatabase = new Database('test'); // CHANGE THIS
 const theServer = new MyServer(theDatabase);
 
-theServer.listen(8080);
+theServer.listen(process.env.PORT);
