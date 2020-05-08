0c6957cc79043dbdaa687b0c64bce0cc0d298c7f
Deemo Farhat <deemofarhat@gmail.com>
2020-05-08 03:33:15 -0400

    updated calendars

--- public/mycalendars.js
+++ public/mycalendars.js
@@ -1,10 +1,46 @@
-function selectItem(id){
+const url = "https://fast-tundra-84247.herokuapp.com";
+
+
+async function postData(url, data) {
+    const resp = await fetch(url,
+                             {
+                                 method: 'POST',
+                                 mode: 'cors',
+                                 cache: 'no-cache',
+                                 credentials: 'same-origin',
+                                 headers: {
+                                     'Content-Type': 'application/json'
+                                 },
+                                 redirect: 'follow',
+                                 body: JSON.stringify(data)
+                             });
+    return resp;
+}
+
+function selectItem(id) {
     var temp = document.getElementById("selected_routine");
     temp.innerHTML=id;
 }
 
+function cal1(id) {
+    let source = "https://calendar.google.com/calendar/embed?src=rp77f3ltpahkasbseu3vam9t9c%40group.calendar.google.com&ctz=America%2FNew_York"
+    document.getElementById("iframe").setAttribute("src", source);
+}
+function cal2(id) {
+    let source = "https://calendar.google.com/calendar/embed?src=sd8a4kqcvfp0lophg151c8krqk%40group.calendar.google.com&ctz=America%2FNew_York"
+    document.getElementById("iframe").setAttribute("src", source);
+}
+function cal3(id) {
+    let source = "https://calendar.google.com/calendar/embed?src=hupbhqvst3i8d8ftgd029i9lds%40group.calendar.google.com&ctz=America%2FNew_York"
+    document.getElementById("iframe").setAttribute("src", source);
+}
+
+document.getElementById("cal1").addEventListener("click", cal1);
+document.getElementById("cal2").addEventListener("click", cal2);
+document.getElementById("cal3").addEventListener("click", cal3);
+
 (async () => {
-    console.log("fetching all workouts from server");
+    console.log("fetching all routines from server");
     const data = {};
     const newURL = url + "/users" + "/routines" + "/readAll";
     const resp = await postData(newURL, data);
